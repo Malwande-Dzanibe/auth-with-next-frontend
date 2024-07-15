f9558d705e0e1f2f0d044cc31206e555d8fa1228
Malwande-Dzanibe <malwandedza@gmail.com>
2024-07-15 16:52:41 +0200

    auth-with-next-frontend

--- server/src/routes/tweetRoute.ts
+++ server/src/routes/tweetRoute.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import Jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -8,10 +9,20 @@ const prisma = new PrismaClient();
 router.post("/", async (req, res) => {
   const content = req.body.content;
 
+  const auth = req.headers["authorization"];
+
+  const jwtoken = auth?.split(" ")[1];
+
+  // try {
+  //@ts-ignore
+  const payLoad = Jwt.verify(jwtoken, "secret");
+
+  console.log(payLoad.id);
+
   const tweet = await prisma.post.create({
     data: {
       content,
-      userId: "5ba1fab9-45fb-468c-a282-20723aba6ae8",
+      userId: "a10feba2-9fe9-44c4-8c10-0f7d8efbc5d7",
     },
     include: {
       user: true,
@@ -19,6 +30,11 @@ router.post("/", async (req, res) => {
   });
 
   res.status(200).json(tweet);
+  // } catch (error) {
+  // res.status(401).json({
+  //   message: error,
+  // });
+  // }
 });
 
 export default router;
