5d1b8b763e3c7c3faef83a168c144e717a2a3687
Malwande-Dzanibe <malwandedza@gmail.com>
2024-08-08 21:25:06 +0200

    auth-with-next-frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import { UserContextWrapper } from "./context/userContextWrapper";
+import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <a rel="icon" href="data: ,"></a>
+      </Head>
       <body className={inter.className}>
         <UserContextWrapper>
           <NavBar />
